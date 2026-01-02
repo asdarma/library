5ec0919554e775b8493d28dcc448331afd8b58fc
Darma <meet.darma@gmail.com>
2026-01-02 22:43:30 +0700

    Added function addbook and div on html

--- library.js
+++ library.js
@@ -1,3 +1,4 @@
+
 function Book(title, author, pages, read) {
     if(!new.target) {
         throw Error("Use the 'new' operator !")
@@ -6,11 +7,23 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID()
     this.info = function() {
-        console.log(`${this.title} by ${this.author}, ${this.pages}, ${this.read}`)
+        console.log(`${this.title} by ${this.author}, ${this.pages}, ${this.read} ID: ${this.id}`)
     };
 };
 
 const theTorah = new Book("The Torah", "Prophet Moses", 1200, "read");
-const thePsalm = new Book("The Psalms", "King David", 200, "read");
-const theRevelation = new Book("The Revelation", "Apostle John", 150, "not read");
+const thePsalm = new Book("The Psalm", "King David", 200, "read");
+const theRev = new Book("The Revelation", "Apostle John", 150, "not read");
+
+const myLIbrary = [];
+
+function addBook(book) {    // 3. Write a function that loops through
+    myLIbrary.push(book);
+}
+
+addBook(theTorah);
+addBook(thePsalm);
+addBook(theRev);
+

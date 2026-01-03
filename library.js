90cd6ed98c7e4a6ad473e2d4332c7d07db0dc53b
Darma <meet.darma@gmail.com>
2026-01-03 14:21:42 +0700

    Fix html table + function displayBook()

--- library.js
+++ library.js
@@ -1,4 +1,5 @@
 
+// book constructor
 function Book(title, author, pages, read) {
     if(!new.target) {
         throw Error("Use the 'new' operator !")
@@ -9,13 +10,14 @@ function Book(title, author, pages, read) {
     this.read = read;
     this.id = crypto.randomUUID()
     this.info = function() {
-        console.log(`${this.title} by ${this.author}, ${this.pages}, ${this.read} ID: ${this.id}`)
+        console.log(`${this.title} by ${this.author}, ${this.pages} pages and ${this.read}. Book ID: ${this.id}`)
     };
 };
 
-const theTorah = new Book("The Torah", "Prophet Moses", 1200, "read");
-const thePsalm = new Book("The Psalm", "King David", 200, "read");
-const theRev = new Book("The Revelation", "Apostle John", 150, "not read");
+// sampe books
+const torah = new Book("The Torah", "Prophet Moses", 1200, "read");
+const psalm = new Book("The Psalm", "King David", 200, "read");
+const rev = new Book("The Revelation", "Apostle John", 150, "not read");
 
 const myLIbrary = [];
 
@@ -23,7 +25,25 @@ function addBook(book) {    // 3. Write a function that loops through
     myLIbrary.push(book);
 }
 
-addBook(theTorah);
-addBook(thePsalm);
-addBook(theRev);
+addBook(torah);     // added book manually to show
+addBook(psalm);
+addBook(rev);
+
+const libraryTable = document.getElementById('libraryTable');   // get the DOM
+
+function displayBook() {
+    myLIbrary.forEach(book => {     // loop on array + dynamicly add then show each data
+        libraryTable.innerHTML += `
+        <tr>
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.page}</td>
+            <td>${book.read}</td>
+            <td>Status</td>
+        </tr>
+        `
+    })
+};
+
+displayBook();
 
